0bc704d14bdd008bf407b234c781d593fb8695d7
nicholasMeyers <s218019@student.aie.edu>
2021-10-25 12:59:49 -0500

    Added an AABB Collider

---
+++ MathForGames/AABBCollider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+
+namespace MathForGames
+{
+    class AABBCollider : Collider
+    {
+        private float _width;
+        private float _height;
+
+        public float Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public float Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public float Left
+        {
+            get { return Owner.Position.X - (Width / 2); }
+        }
+
+        public float Right
+        {
+            get  { return Owner.Position.X + (Width / 2); }
+        }
+
+        public float Top
+        {
+            get  {  return Owner.Position.Y - (Width / 2); }
+        }
+
+        public float Bottom
+        {
+            get  { return Owner.Position.Y - (Width / 2); }
+        }
+
+        public AABBCollider(float width, float height, Actor owner) : base(owner, ColliderType.AABB)
+        {
+            _width = width;
+            _height = height;
+        }
+    }
+}
